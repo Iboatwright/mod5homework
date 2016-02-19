da3dfa70530750909eacb7a6d7a3ee2572de57ab
Ivan Boatwright <iboatwright@users.noreply.github.com>
2016-02-19 01:47:14 -0500

    main runs. displayed output is very basic and will probably be marked down.

--- factorial_calculator.py
+++ factorial_calculator.py
@@ -1,7 +1,117 @@
-f  = 1
+# factorial_calculator.py
+# Exercise selected: Chapter 5 program 12
+# Name of program: Factorial Calculator
+# Description of program: this program will calculate n! where n is some
+#   nonnegative integer input by the user.
+# Ivan Boatwright
+# February 18, 2016 (doomsday tomorrow)
 
-def calc_f(n, v=1):
-    if n <= 1:
-        return v
+def main():
+    # Variable definitions
+    nObjectCount = 0    # Nonnegative number (the n for n!)
+    nFactorial = 0      # Resulting permutation count (n!)
+    
+    # Call the user introduction module
+    fluffy_intro()
+    
+    # Get the a non-negative number of Objects from the user
+    nObjectCount = get_valid_inputs([['nonnegative integer',
+                   'nonnegative number to calculate']])
+
+    # Calculate the factorial of nObjectCount
+    nFactorial = calc_factorial(nObjectCount)
+
+    # Display calculated factorial to user
+    display_results(nObjectCount, nFactorial)
+
+    # End of main.
+    return None
+
+
+# Displays an introduction to the program and describes what it does.
+def fluffy_intro():
+    print('Welcome to the n! Factorial Calculator.')
+    print('This program will calculate factorials of nonnegative integers.')
+    print('It then displays the results for your viewing pleasure.\n')
+    return None
+
+
+# get_valid_inputs requests input from the user then tests the input.
+#   If invalid, it will alert the user and request the correct input.
+# The parameter is a nested List of ordered pair Lists.
+#   First value is the validation test and second is the user prompt.
+def get_valid_inputs(requestsList):
+    # local List to hold user inputs for return to calling module
+    userInputs = []
+
+    # Loop through each entry in requestList assigning each List pair
+    #  to request.
+    for request in requestsList:
+        # untestedInput is a holding variable for testing user input validity.
+        # First user prompt before testing loop
+        untestedInput = int(prompt_user_for_input(request[1]))
+
+        # If test_value returns True, Not converts it to False and the While
+        #  Loop will not execute.
+        # If test_value returns False, the While executes and the user is
+        #  prompted to enter a valid value.
+        while (not test_value(request[0], untestedInput)):
+
+            print(untestedInput, 'is not a valid value.')
+            untestedInput = (prompt_user_for_input(request[1]))
+
+        # The user input tested valid and is appended to the userInputs List.
+        userInputs.append(untestedInput)
+    # for loop terminates and userInputs are returned to calling Module
+    return userInputs[0]  # Only one value to return in this program.
+
+
+
+# prompt_user_for_item is passed a String to print to screen as part of a user
+#   prompt.  Then it converts the user input into a real number and returns
+#   it to the calling module.
+def prompt_user_for_input(promptTerm):
+    # promptTerm is a local variable to hold the value passed from the
+    #   calling module.
+    print('Please enter your {}.'.format(promptTerm))
+    return int(input('  >> '))
+
+
+
+# test_value uses the testCondition to select the proper test.
+# It returns True or False to the calling Module.
+def test_value(testCondition, testItem):
+    # The If-Then-Else structure functions as a Switch for test selection.
+    if testCondition == 'nonnegative integer':
+        # Is testItem an Integer? If so, then is it also greater than
+        #  or equal to 0?
+        if (isinstance(testItem, int) and (testItem >= 0)):
+            return True
+        else:
+            return False
     else:
-        return calc_f(n - 1, v * n)
+        return None
+
+
+# calc_factorial uses recursion to repeatedly call itself while decrementing
+#  nCount and multiplying fValue by nCount.  When nCount is less than or
+#  equals 1 only fValue is returned.  nCount can equal 0 because 0! equals 1.
+#  Recursion isn't very efficient in Python and has a limit to the number of
+#  recursive calls that can be made.
+def calc_factorial(nCount, fValue=1):
+    if nCount <= 1:
+        return fValue
+    else:
+        return calc_factorial(nCount - 1, fValue * nCount)
+
+
+# display_results is passed values used in print statements to display
+#  the results of the program to the user.
+def display_results(nObjects, nFactorial):
+    print('\n')
+    print('The factorial of {} is {}.'.format(nFactorial, nObjects))
+    return None
+
+
+# Call the main function to run the program.
+main()
